8f23c7b5fe300164a46e0955cde8b15c845a69a1
nomeo-310 <onome962@gmail.com>
2024-07-28 06:00:40 +0100

    folders created and auth add

---
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,46 @@
+import { AuthOptions } from 'next-auth'
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '@/lib/prisma'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcrypt'
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as any,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: {label: 'email', type: 'text'},
+        password: {label: 'password', type: 'password'}, 
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid Credentials');
+        }
+
+        const user = await prisma.user.findUnique({where: {email: credentials.email}});
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid Credentials')
+        }
+
+        const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
+        if (!isCorrectPassword) {
+          throw new Error('Wrong Password')
+        }
+
+        return user
+      }
+    }),
+  ],
+  pages: {
+    signIn: '/sign-in'
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: { strategy: 'jwt'},
+  secret: process.env.NEXTAUTH_SECRET,
+}
